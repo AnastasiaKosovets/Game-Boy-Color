a54297a2fb9e532a0964d9c0c68e19504a78cda7
Anastasia <anastasiakosovets@gmail.com>
2023-04-28 10:54:34 +0200

    changes in css code(simplified)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-function encenderPantalla(){
+function onScreenNintendo(){
     let screen = document.getElementById("idScreen");
     screen.classList.add("screenOn");
     
+ }
+
+
+ function prueba() {
+    let ledPower = document.getElementById("switchOn");
+    ledPower.classList.add("ledPowerOn");
+ }
+
+function startAll() {
+    onScreenNintendo();
+    prueba();
 }
